ad01bcfc5dcf0ef56510e9d7c0d5ec9e9ded10dd
sixteamparlay <genekowalski@hotmail.com>
2017-02-17 16:32:22 -0500

    Update faCalendarPlugin.js
    typo?

--- www/faCalendarPlugin.js
+++ www/faCalendarPlugin.js
@@ -3,7 +3,7 @@ var faCalendar = {
         cordova.exec(successCallback, errorCallback, "WPCordovaClassLib.Cordova.Commands.faCalendar", "GetDescription", []);
     },
 	AddEvent: function(successCallback, errorCallback, notes, location, title, year, month, day, duration){
-		var strInput = notes + '|' + title + '|' + location + '|' + year + '|' + month + '|' + day + '|' + duration' + '|' + ' ';
+		var strInput = notes + '|' + title + '|' + location + '|' + year + '|' + month + '|' + day + '|' + duration + '|' + ' ';
 		
 		cordova.exec(successCallback, errorCallback, "WPCordovaClassLib.Cordova.Commands.faCalendar", "AddEvent", [strInput]);
 	}
